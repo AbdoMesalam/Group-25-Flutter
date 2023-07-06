c81b93e9bb06dfcaf994112f10adfda3ff2a822f
AbdoMesalam <138803336+AbdoMesalam@users.noreply.github.com>
2023-07-06 19:39:11 +0300

    Update Task 2.dart

--- Flutter-G25/Sessions/Session2/Task 2/Task 2.dart
+++ Flutter-G25/Sessions/Session2/Task 2/Task 2.dart
@@ -1,5 +1,11 @@
 void main() {
-  int score = 80; // Replace with your own score value
+
+
+//program takes variable for degree out of 100 and print
+// grade(A,B،C,D) using switch
+
+
+  int score = 57;
 
   String grade;
 
@@ -23,4 +29,33 @@ void main() {
   }
 
   print('Your score is $score out of 100, which corresponds to a grade of $grade.');
+
+  
+  //program takes 3 variables and print the average
+  
+
+  double num1 = 14.0;
+  double num2 = 87.0;
+  double num3 = 16.5;
+
+  double average = (num1 + num2 + num3) / 3;
+
+  print('The average of $num1, $num2, and $num3 is $average.');
+
+
+  //program takes 2 variables and compare between them
+  
+
+  int number1 = 47;
+  int number2 = 12;
+
+  if (num1 > num2) {
+    print('$num1 is greater than $num2.');
+  } else if (num2 > num1) {
+    print('$num2 is greater than $num1.');
+  } else {
+    print('$num1 and $num2 are equal.');
+  }
+  
+  
 }
