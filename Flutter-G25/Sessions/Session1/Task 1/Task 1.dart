02165efcb51c59cc0856e9ea1803b5cd0313ecf6
unknown <abdomesalam@gmail.com>
2023-07-06 19:17:03 +0300

    task 1 finished

---
+++ Flutter-G25/Sessions/Session1/Task 1/Task 1.dart
@@ -0,0 +1,23 @@
+void main(){
+
+
+  // covert degree from Fahrenheit to Celsius
+
+
+  double fahrenheit = 300.0;
+
+  double celsius = (fahrenheit - 32) * 5 / 9;
+
+  print('$fahrenheit degrees Fahrenheit is $celsius degrees Celsius.');
+
+
+  // calculate area of circle
+
+
+  double radius = 14.0;
+
+  double area = 3.14 * radius * radius;
+
+  print('The area of the circle is $area square units.');
+
+}
